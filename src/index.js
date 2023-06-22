5f5f29d7c9de2eaec3f0a3f8a39cff52e7260769
Codeworm08 <siddhantmajumder@gmail.com>
2023-06-23 00:48:33 +0530

    Revert "created navbar and made component for signing out"
    This reverts commit 3dcaa36c44630d9fcff81f75cf608fbd7a49a87f.
    Go back to using button for log out because js window keeps popping up repeatedly and does not redirect.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider} from './UserContext';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserProvider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    </UserProvider>
   </React.StrictMode>
  
 );
